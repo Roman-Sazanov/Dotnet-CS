fa21545b80c05b2a27a0ec434594695f5cb50b10
Roman Sazanov <r.v.1990@yandex.ru>
2023-06-04 19:14:27 +0300

    Added tasks 19,23

---
+++ task19/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+int Palindrome(int num)
+{
+    if(num < 10000 || num >= 100000)   
+    {
+        Console.WriteLine("Введённое число не является пятизначным");
+    }
+    else
+    {
+        int dig1 = num / 10000;
+        int dig2 = num / 1000 % 10;
+        int dig4 = num % 100 / 10;
+        int dig5 = num % 10;
+        if(dig1 == dig5 || dig2 == dig4)
+        {
+            Console.WriteLine("Число является палиндромом");
+        }
+        else
+        {
+            Console.WriteLine("Число не является палиндромом");
+        }
+    }
+    return num;
+}
+
+Console.WriteLine("Введите пятизначное число: ");
+int number = int.Parse(Console.ReadLine());
+Palindrome(number);
---
+++ task23/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+int CubeTable(int num)
+{
+    if(num < 1)
+    {
+        Console.WriteLine("Введённое число не соответствует условию задачи");
+    }
+    else
+    {
+        int index = 1;
+        Console.Write("Таблица кубов:");
+        while(index <= num)
+        {
+            int cube = index * index * index;
+            Console.Write(" " + cube);
+            index = index + 1;
+        }
+    }
+    return num;
+}
+
+Console.WriteLine("Введите число не менее 1:");
+int number = int.Parse(Console.ReadLine());
+CubeTable(number);
